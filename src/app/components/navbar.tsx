eb8cde0553773fc01f1a626d4008879bb93967e8
Matthew Zahm <matthew.zahm31@gmail.com>
2024-07-15 17:29:44 -0600

    base layout

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -8,9 +8,9 @@ import Image from 'next/image';
 
 export default function NavBar() {
   return (
-    <AppBar position="fixed" className='h-14 text-slate-900 shadow-none border-b-2 border-b-slate-200' >
+    <AppBar position="fixed" className='h-14 text-slate-900 shadow-none border-b-2 border-b-slate-300' >
       <Container className='w-full m-0 p-0 max-h-14 max-w-full'>
-        <Toolbar className='w-full max-h-14 min-h-14 border-b-2 border-b-slate-200'>
+        <Toolbar className='w-full max-h-14 min-h-14 border-b-2 border-b-slate-300'>
           <Grid container justifyContent="space-between" className='w-full'>
             <Grid item xs={3} container className='items-center h-12'>
               <Image
@@ -26,21 +26,25 @@ export default function NavBar() {
             </Grid>
             <Grid item xs={6} container justifyContent="space-around" className='items-center'>
               <Grid item>
-                <Grid container>
+                <Grid container className='bg-eco-green-light p-1 pl-5 pr-5 rounded-3xl'>
                   <Typography
-                    className='m-2 mobile-hide max-w-full'
+                    className='m-2 mobile-hide max-w-full bg-eco-green-light'
                   >
                     Search:
                   </Typography>
                   <input
-                    className='border-2 border-slate-200 rounded-lg h-10 p-2 focus:outline-none'
+                    className='border-2 border-slate-300 rounded-lg h-10 p-2 focus:outline-none'
                     type='text'
                   />
                 </Grid>
               </Grid>
             </Grid>
             <Grid item xs={3} container className='items-center flex-row-reverse'>
-              <Typography>Login</Typography>
+              <Typography
+                className='bg-eco-green-light p-2 pr-5 pl-5 rounded-2xl border-2 border-slate-300'
+              >
+                Login
+              </Typography>
             </Grid>
           </Grid>
         </Toolbar>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,51 @@
-import Image from "next/image";
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { Box, Paper } from "@mui/material";
+import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
+import ShareIcon from '@mui/icons-material/Share';
+import CommentIcon from '@mui/icons-material/Comment';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import PersonIcon from '@mui/icons-material/Person';
+import * as React from 'react';
+
+// export default function IconButtons() {
+//   return (
+//     <Stack direction="row" spacing={1}>
+//     </Stack>
+//   );
+// }
 
 
 export default function Home() {
-  var tile_1 = "Valley of Reflection";
-
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Grid container direction="row" className="w-full justify-between">
-          <Grid item container direction="column" xs={2} className="border-2 border-indigo-600 rounded">
-            <Grid item className="h-96">
-              
+      <main className="flex min-h-screen flex-col items-center pb-4 pt-24 pl-12 pr-12">
+        <Grid id="feed" container direction="column" className="w-full h-dvh">
+          <Grid item container direction="row" className="feed-item w-full h-dvh justify-around ">
+            <Grid item container className="relative feed-item__contents border-2 bg-eco-green-light border-slate-300 max-w-screen-md rounded-lg">
+
+              <Grid item container direction="column" className="feed-item__toolbar absolute">
+                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
+                  <PersonIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
+                </Grid>
+                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
+                  <EnergySavingsLeafIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
+                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
+                </Grid>
+                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
+                  <CommentIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
+                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
+                </Grid>
+                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
+                  <BookmarkIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
+                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
+                </Grid>
+                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
+                  <ShareIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
+                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography className="text-center p-5">{tile_1}</Typography>
-            </Grid>
-          </Grid>
-          <Grid item container xs={2} className="bg-slate-200 border-2 border-indigo-600 rounded">
-            <Typography>Grove of Growth</Typography>
-          </Grid>
-          <Grid item container xs={2} className="border-2 border-indigo-600 rounded">
-            <Typography>Summit of Strength</Typography>
-          </Grid>
-          <Grid item container xs={2} className="border-2 border-indigo-600 rounded">
-            <Typography>Waters of Renewal</Typography>
           </Grid>
         </Grid>
       </main>
