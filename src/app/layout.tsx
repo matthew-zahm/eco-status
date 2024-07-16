55da6d01163b83b25062848d6d0e220a4c587322
Matthew Zahm <matthew.zahm31@gmail.com>
2024-07-16 11:23:56 -0600

    sidebar and responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/navbar";
+import { Grid, Typography, SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreIcon from '@mui/icons-material/Explore';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AddIcon from '@mui/icons-material/Add';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +17,24 @@ export const metadata: Metadata = {
   description: "Improve yourself and the environment!",
 };
 
+interface Side_Item_Props {
+  icon?: OverridableComponent<SvgIconTypeMap>,
+  title: string
+}
+
+export function Side_Item(props: Side_Item_Props) {
+  return <>
+    <Grid item container direction="row" className="mb-4">
+      { props.icon &&
+        <props.icon className="side_item__icon mr-2 text-eco-green text-3xl" />
+      }
+      <Typography className="side_item__text font-bold text-lg mt-1 mobile-hide">
+        {props.title}
+      </Typography>
+    </Grid>
+  </>;
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,9 +42,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} relative`}>
         <NavBar />
-        {children}
+        <Grid container direction="row">
+          <Grid item direction="column" id="side-bar" className="h-full p-5 ">
+            <Side_Item icon={HomeIcon} title="For You" />
+            <Side_Item icon={ExploreIcon} title="Discover" />
+            <Side_Item icon={BookmarkIcon} title="Following" />
+            <Side_Item icon={AccountCircleIcon} title="Profile" />
+            <Side_Item icon={AddIcon} title="Create" />
+          </Grid>
+          <Grid item id="page">
+            {children}
+          </Grid>
+        </Grid>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
@@ -5,46 +6,44 @@ import ShareIcon from '@mui/icons-material/Share';
 import CommentIcon from '@mui/icons-material/Comment';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import PersonIcon from '@mui/icons-material/Person';
-import * as React from 'react';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { SvgIconTypeMap } from '@mui/material';
 
-// export default function IconButtons() {
-//   return (
-//     <Stack direction="row" spacing={1}>
-//     </Stack>
-//   );
-// }
+
+interface Toolbar_Props {
+  icon: OverridableComponent<SvgIconTypeMap>,
+  count?: number
+}
+
+export function Toolbar_Item(props: Toolbar_Props) {
+  return <>
+    <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
+      <props.icon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
+      {props.count != undefined &&
+        <Typography className="feed-item__toolbar-count mt-12 w-full text-center">{props.count}</Typography>
+      }
+    </Grid>
+  </>
+}
 
 
 export default function Home() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center pb-4 pt-24 pl-12 pr-12">
+      <main id="main" className="flex min-h-screen flex-col items-center pb-4 pt-20 pl-8 pr-8">
         <Grid id="feed" container direction="column" className="w-full h-dvh">
           <Grid item container direction="row" className="feed-item w-full h-dvh justify-around ">
             <Grid item container className="relative feed-item__contents border-2 bg-eco-green-light border-slate-300 max-w-screen-md rounded-lg">
 
               <Grid item container direction="column" className="feed-item__toolbar absolute">
-                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
-                  <PersonIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
-                </Grid>
-                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
-                  <EnergySavingsLeafIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
-                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
-                </Grid>
-                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
-                  <CommentIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
-                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
-                </Grid>
-                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
-                  <BookmarkIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
-                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
-                </Grid>
-                <Grid item container direction="column" className="feed-item__toolbar-item w-12 h-12 rounded-full  bg-eco-green-light relative mb-8">
-                  <ShareIcon className="feed-item__toolbar-icon m-auto bg-eco-green-light text-eco-green" />
-                  <Typography className="feed-item__toolbar-count mt-12 w-full text-center">40</Typography>
-                </Grid>
+                <Toolbar_Item icon={PersonIcon} />
+                <Toolbar_Item icon={EnergySavingsLeafIcon} count={40} />
+                <Toolbar_Item icon={CommentIcon} count={50} />
+                <Toolbar_Item icon={BookmarkIcon} count={24} />
+                <Toolbar_Item icon={ShareIcon} count={11} />
               </Grid>
+
             </Grid>
           </Grid>
         </Grid>
